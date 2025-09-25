0f800a6bf36a69adb9f3b230b65bfb93f65bb58c
David Adamson <david.adamson2304@gmail.com>
2025-09-25 18:26:43 +0200

    refactor: split esmf-vue implementation into files

---
+++ packages/esmf-vue/src/create-microfrontend.ts
@@ -0,0 +1,23 @@
+import { MF } from "esmf-js";
+import { createApp } from "vue";
+
+type App = ReturnType<typeof createApp>;
+
+export type Options<P> = {
+  component: any;
+  init(app: App, props: P): void;
+};
+
+export function createMicrofrontend<P>(opts: Options<P>): MF<P> {
+  let app: App;
+  return {
+    mount(domElement: Element, props: P) {
+      app = createApp(opts.component);
+      opts.init(app, props);
+      app.mount(domElement);
+    },
+    unmount() {
+      app.unmount();
+    },
+  };
+}
